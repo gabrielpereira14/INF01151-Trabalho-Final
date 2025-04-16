3c48d1b50ffa90eed4591345a9ae5bb466b523bd
Doguinhoo <leo.ribeiropoa@gmail.com>
2025-04-16 09:39:14 -0300

    Cria a maquete do servidor

--- src/server/server.c
+++ src/server/server.c
@@ -1,3 +1,17 @@
-int main(){
+int main() {
+    // Cria o socket
+
+    // Faz bind no port de conecção da interface
+
+    while (1) {
+        // Espera uma requisição de conecção
+
+        // Verifica se a conecção é válida e responde para o cliente
+
+        // Se válida, cria os sockets de receber e enviar arquivos
+
+        // Guarda os dados da conecção e lança as threads
+    }
+    
     return 0;
 }
