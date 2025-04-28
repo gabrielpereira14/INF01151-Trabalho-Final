84cb9922b85905a61556c769a232c58dccf2af10
Doguinhoo <leo.ribeiropoa@gmail.com>
2025-04-28 13:27:57 -0300

    Separa os sockets de push e receive, move a criação dos sockets de espera de conecção (push e receive) pra fora do loop principal

--- src/server/server.c
+++ src/server/server.c
@@ -8,7 +8,8 @@
 #include <pthread.h>
 
 #define PORT_INTERFACE 4000
-#define PORT_TRANSFER 4001
+#define PORT_PUSH 4001
+#define PORT_RECEIVE 4002
 #define MAX_USERNAME_LENGTH 32
 const int ANSWER_OK = 1;
 
@@ -18,24 +19,49 @@ void *push(void* sock); // Envia os arquivos para o cliente
 void *receive(void* sock); // Recebe os arquivos do cliente
 
 int main() {
-    // Cria o socket
-	int sock_interface_listen;
+    // Cria os sockets de espera de conecção
+	int sock_interface_listen, sock_push_listen, sock_receive_listen;
 	
 	if ((sock_interface_listen = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-        perror_exit("ERRO abrindo o socket de espera de coneccoes pela interface: ");
+        perror_exit("ERRO abrindo o socket de espera de coneccoes de interface: ");
 
-    // Faz bind no port de conecção da interface
-	struct sockaddr_in serv_addr;
+	if ((sock_push_listen = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+        perror_exit("ERRO abrindo o socket de espera por coneccao de push: ");
+		
+	if ((sock_receive_listen = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+        perror_exit("ERRO abrindo o socket de espera por coneccao de receive: ");
 
-    serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT_INTERFACE);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(serv_addr.sin_zero), 8);
+    // Faz bind nos ports
+	struct sockaddr_in interface_serv_addr, push_serv_addr, receive_serv_addr;
 
-    if (bind(sock_interface_listen, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    interface_serv_addr.sin_family = AF_INET;
+	interface_serv_addr.sin_port = htons(PORT_INTERFACE);
+	interface_serv_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(interface_serv_addr.sin_zero), 8);
+
+    push_serv_addr.sin_family = AF_INET;
+    push_serv_addr.sin_port = htons(PORT_PUSH);
+    push_serv_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(push_serv_addr.sin_zero), 8);
+
+	receive_serv_addr.sin_family = AF_INET;
+    receive_serv_addr.sin_port = htons(PORT_RECEIVE);
+    receive_serv_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(receive_serv_addr.sin_zero), 8);
+
+    if (bind(sock_interface_listen, (struct sockaddr *) &interface_serv_addr, sizeof(interface_serv_addr)) < 0) 
 		perror_exit("ERRO vinculando o socket de espera de coneccoes pela interface: ");
+
+	if (bind(sock_push_listen, (struct sockaddr *) &push_serv_addr, sizeof(push_serv_addr)) < 0) 
+		perror_exit("ERRO vinulando o socket de espera da coneccao de push: ");
+		
+	if (bind(sock_receive_listen, (struct sockaddr *) &receive_serv_addr, sizeof(receive_serv_addr)) < 0) 
+			perror_exit("ERRO vinulando o socket de espera da coneccao de receive: ");
 	
+	// Começa a esperar pedidos de conecção
 	listen(sock_interface_listen, 5);
+	listen(sock_push_listen, 1);
+	listen(sock_receive_listen, 1);
 
     while (1) {
         // Espera uma requisição de conecção
@@ -56,40 +82,30 @@ int main() {
 
         // TODO: verificar se o usuário já tem dois dispositivos conectados
 
-        // Se válida, cria o socket de transferência
-	    struct sockaddr_in transfer_serv_addr;
-        int sock_transfer_listen, sock_transfer;
-		struct sockaddr_in cli_transfer_addr;
-	    socklen_t cli_transfer_len;
-
-    	if ((sock_transfer_listen = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-            perror_exit("ERRO abrindo o socket de espera por coneccao de transferencia: ");
-
-        transfer_serv_addr.sin_family = AF_INET;
-	    transfer_serv_addr.sin_port = htons(PORT_TRANSFER);
-	    transfer_serv_addr.sin_addr.s_addr = INADDR_ANY;
-	    bzero(&(transfer_serv_addr.sin_zero), 8);
-
-		if (bind(sock_transfer_listen, (struct sockaddr *) &transfer_serv_addr, sizeof(transfer_serv_addr)) < 0) 
-			perror_exit("ERRO vinulando o socket de espera da coneccao de transferencia: ");
-	
-		listen(sock_transfer_listen, 1);
-
- 	    if ((sock_transfer = accept(sock_transfer_listen, (struct sockaddr *) &cli_transfer_addr, &cli_transfer_len)) == -1) 
-		    perror_exit("ERRO aceitando a coneccao de transferencia");
-
-		// Comunica pra o usuário fazer a conecção
+		// Comunica pra o cliente fazer a conecção
 		if (write(sock_interface, &ANSWER_OK, sizeof(ANSWER_OK)) != sizeof(ANSWER_OK))
-			perror_exit("ERRO respondendo para o cliente");
+			perror_exit("ERRO respondendo para o cliente: ");
+
+		// Cria os socket de transferência
+	    int sock_push, sock_receive;
+		struct sockaddr_in cli_push_addr, cli_receive_addr;
+    	socklen_t cli_push_addr_len, cli_receive_addr_len;
+
+		// Aceita as conecções
+		if ((sock_push = accept(sock_push_listen, (struct sockaddr *) &cli_push_addr, &cli_push_addr_len)) == -1) 
+		    perror_exit("ERRO aceitando a coneccao de push: ");
+
+		if ((sock_receive = accept(sock_receive_listen, (struct sockaddr *) &cli_receive_addr, &cli_receive_addr_len)) == -1) 
+		    perror_exit("ERRO aceitando a coneccao de receive: ");
+
+		// TODO: Guarda os dados da conecção
 
         // Lança as threads
 		pthread_t interface_thread, push_thread, receive_thread;
 
 		pthread_create(&interface_thread, NULL, interface, (void*)sock_interface);
-		pthread_create(&push_thread, NULL, push, (void*)sock_transfer);
-		pthread_create(&receive_thread, NULL, receive, (void*)sock_transfer);
-
-		// Guarda os dados da conecção
+		pthread_create(&push_thread, NULL, push, (void*)sock_push);
+		pthread_create(&receive_thread, NULL, receive, (void*)sock_push);
     }
     
     return 0;
